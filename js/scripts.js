4d2cb09e5b786ea1be24a4818cf909860d7c4d4d
Luke Vandekieft and William Wittenbrock <luke@>
2018-08-27 13:53:44 -0700

    Add character creation feature.

--- js/scripts.js
+++ js/scripts.js
@@ -20,16 +20,48 @@ var dice = function() {
 // FRONTEND
 $(document).ready(function(){
 
-  var newCharacter = new Character($(".nameInput"));
-
+// WHEN THE NAME BUTTON IS CLICKED:
   $("button#nameButton").click(function(){
+    $("#createName").hide();
+    $("#createStats").show();
+    var characterName = $(".nameInput").val();
+    $("#characterName").text(characterName);
+    var newCharacter = new Character (characterName)
+    console.log(newCharacter);
+    // for (i=0;i<4;i++){
+    //   if (i == 0){
+    $("#rollText").text("Roll for Strength.")
+        console.log("hi");
 
+      var i = 0
+
+// WHEN THE ROLL DICE BUTTON IS CLICKED:
+      $("button#diceRollButton").click(function(){
+        if (i == 0){
+          newCharacter.strength = dice();
+          $("#statResultDisplay").show();
+          $("#strengthStat").text(newCharacter.strength)
+          $("#statResultDisplay").text("Your character's strength is " + newCharacter.strength + ".")
+          $("#rollText").text("Roll for agility.")
+        } else if (i == 1){
+          newCharacter.agility = dice();
+
+          $("#agilityStat").text(newCharacter.agility)
+          $("#statResultDisplay").text("Your character's agility is " + newCharacter.agility + ".")
+          $("#rollText").text("Roll for intelligence.")
+
+        } else if (i == 2){
+          newCharacter.intelligence = dice();
+          $("#intelligenceStat").text(newCharacter.intelligence)
+          $("#statResultDisplay").text("Your character's intelligence is " + newCharacter.intelligence + ".")
+          $("#rollText").text("Roll for charisma.")
+        } else if (i == 3){
+          newCharacter.charisma = dice();
+          $("#charismaStat").text(newCharacter.charisma)
+          $("#statResultDisplay").text("Your character's charisma is " + newCharacter.charisma + ".")
+          $("#rollText").text("Your journey is about to begin...")
+        }
+        i ++;
+    });
   });
-
- $("button#diceRollButton").last().click(function(){
-   $("stengthStat").text()
-
- });
-
-
 });

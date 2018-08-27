4f7ce12cb97c395a4949699b14baca35ea20d8c3
Colin Cook and Eric Conner <eric@>
2018-08-27 12:04:18 -0700

    Add more story elements and button click functions.

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,19 @@
 
 // FRONTEND
 $(document).ready(function(){
+  $("button#companion-choice-one").click(function(event){
+    $("#page-two-a").show();
+    $("#page-one").hide();
+});
+
+  $("button#companion-choice-two").click(function(event){
+    $("#page-two-b").show();
+    $("#page-one").hide();
+  });
 
 
 
 
 
 
-  
 });

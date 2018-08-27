3025740e298b07c040e7d38becf31035d43b651f
Luke Vandekieft and William Wittenbrock <william@>
2018-08-27 10:50:17 -0700

    Create Character constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // BACKEND
 
+// CHARACTER CONSTRUCTOR
+
+function Character (name, strength, agility, intelligence, charisma) {
+  this.name = name;
+  this.strength = strength;
+  this.agility = agility;
+  this.intelligence = intelligence;
+  this.charisma = charisma
+}
+
+
+// DICE ROLL FUNCTION THAT GENERATES A NUMBER BETWEEN 1-20
 var dice = function() {
   var roll = Math.floor((Math.random() *20) +1);
   return roll;

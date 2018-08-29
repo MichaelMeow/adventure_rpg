6790fd1255c8d0c39086c3af54f5546078b2e859
Luke Vandekieft and William Wittenbrock <luke@>
2018-08-29 11:30:48 -0700

    Add universal check stat button.

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,30 @@ var showText = function (target, message, index) {
     $(".current-story").show();
     $(".hidden").hide();
   }
-
 }
 
+var showTextStats = function (target, message, index) {
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showTextStats(target, message, index); }, 5);
+  } else {
+    $(".hidden").hide();
+  }
+}
 
+var statCheck = function (characterStat, statCheckNumber, characterRoll) {
+  if (statCheckNumber > (characterRoll + characterStat)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// WORKING SOLUTION TO MAKE ASCII ART LOOK BETTER
+// function reverseString(string) {
+//   return string.split("").reverse().join("");
+// }
+//
 
 
 
@@ -45,7 +65,7 @@ var showText = function (target, message, index) {
 // }
 
 $(document).ready(function(){
-var storyArray = [[2,3],[4,5],[5,6],[7,5],[9,10],[5,8],[0,0],[0,0]]
+var storyArray = [[2,3],[4,5,9,10],[5,6],[7,5],[9,10],[5,8],[0,0],[0,0]]
 
 $(".container-fluid").hide();
 $("button#playNow").click(function(){
@@ -53,7 +73,17 @@ $(".splash").hide();
 $(".container-fluid").show();
 })
 
-
+var checkedCharStat = function(buttonClass, character) {
+  if (buttonClass.includes("strength")) {
+    return character.strength;
+  } else if (buttonClass.includes("agility")) {
+    return character.agility;
+  } else if (buttonClass.includes("charisma")) {
+    return character.charisma;
+  } else {
+    return character.intelligence;
+  }
+};
 
 // WHEN THE NAME BUTTON IS CLICKED:
   $("button#nameButton").click(function(){
@@ -132,5 +162,100 @@ $(".container-fluid").show();
       showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
     })
 
+    $(".stat").click(function(){
+      $(".printText").empty();
+      var rollRequired = parseInt($(this).val())
+      var buttonClass = $(this).attr("class");
+      var checkedStat = checkedCharStat(buttonClass, newCharacter)
+      console.log(checkedStat);
+      showTextStats($(".printText"), 'You need a total of ' + rollRequired + ' strength in order to pass this roll.', 0);
+      $(".current-story").hide(); //still showing because showtext waits and shows current story
+      $(".testButton").hide();
+      $('.testButton').append('<button id="statCheckButton" type="button">Roll!</button>')
+      setTimeout(function () { $(".testButton").show(); }, 750);
+
+        $("#statCheckButton").click(function() {
+          var currentRoll = dice();
+
+          $(".testButton").empty();
+          $(".printText").append("<br>")
+          $('.testButton').append('<button id="statRollButton" type="button">Ok</button>')
+          showTextStats($(".printText"), 'Your roll was ' + currentRoll + "." + " Your total strength is " + (currentRoll + checkedStat) + ".", 0);
+
+          $("#statRollButton").click(function() {
+            $('.testButton').empty();
+            if (buttonClass.includes("option1")){
+              $(".hidden-story").append($("#" + currentPage))
+              if (statCheck(checkedStat, rollRequired, currentRoll) === true) {
+              var aPage = storyArray[currentPage-1][0]
+            } else {
+                var aPage = storyArray[currentPage-1][1]
+              }
+              $(".current-story").append($("#" + aPage))
+              currentPage = aPage
+              $(".printText").empty();
+              showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+            }
+            if (buttonClass.includes("option2")){
+              $(".hidden-story").append($("#" + currentPage))
+              if (statCheck(checkedStat, rollRequired, currentRoll) === true) {
+              var aPage = storyArray[currentPage-1][2]
+            } else {
+                var aPage = storyArray[currentPage-1][3]
+              }
+              $(".current-story").append($("#" + aPage))
+              currentPage = aPage
+              $(".printText").empty();
+              showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+            }
+            if (buttonClass.includes("option3")){
+              $(".hidden-story").append($("#" + currentPage))
+              if (statCheck(checkedStat, rollRequired, currentRoll) === true) {
+              var aPage = storyArray[currentPage-1][4]
+            } else {
+                var aPage = storyArray[currentPage-1][5]
+              }
+              $(".current-story").append($("#" + aPage))
+              currentPage = aPage
+              $(".printText").empty();
+              showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+            }
+            if (buttonClass.includes("option4")){
+              $(".hidden-story").append($("#" + currentPage))
+              if (statCheck(checkedStat, rollRequired, currentRoll) === true) {
+              var aPage = storyArray[currentPage-1][6]
+            } else {
+                var aPage = storyArray[currentPage-1][7]
+              }
+              $(".current-story").append($("#" + aPage))
+              currentPage = aPage
+              $(".printText").empty();
+              showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+            }
+          })
+        })
+      })
+    });
+
+
+    // $(".agility").click(function(){
+    //   $(".current-story").hide();
+    //   $(".hidden-story").append($("#" + currentPage))
+    //   if (statCheck(newCharacter.agility, 25) === true) {
+    //     var aPage = storyArray[currentPage-1][0]
+    //     $(".current-story").append($("#" + aPage))
+    //     currentPage = aPage
+    //     console.log(currentPage)
+    //     $(".printText").empty();
+    //     showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+    //   } else {
+    //     var bPage = storyArray[currentPage-1][1]
+    //     $(".current-story").append($("#" + bPage))
+    //     currentPage = bPage
+    //     console.log(currentPage)
+    //     $(".printText").empty();
+    //     showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
+    //   }
+    // })
+
   });
-});

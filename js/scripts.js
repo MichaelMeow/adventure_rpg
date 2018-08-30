2672f42a3800273f6a21f928a1193624453fc672
Luke Vandekieft and William Wittenbrock <william@>
2018-08-30 11:30:37 -0700

    Troubleshoot button display.

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ var dice = function() {
 };
 
 var showText = function (target, message, index) {
+  message.replace(/&nbsp;/g,' ');
   if (index < message.length) {
     $(target).append(message[index++]);
     setTimeout(function () { showText(target, message, index); }, 5);
@@ -67,7 +68,7 @@ var statCheck = function (characterStat, statCheckNumber, characterRoll) {
 //INN 1-7//
 var storyArray = [[2,3],[4,9],[8,5],[9,6],[8,7],[0,0],[0,0],
 //FOREST 8-20//
-[10],[11],[19,12],[20,13],[15,18,14,18],[17,18,16,18],[49],[49],[21],[21],[0,0],[40],[29],
+[10],[11],[19,12],[20,13],[15,18,14,18],[17,18,16,18],[48],[48],[21],[21],[0,0],[40],[29],
 //castle 21-58//
 [23,22],[24,25],[24,25],[34,24,34,23,34,24,34,24],[27,28],[34,26,34,26,34,26,34,26],[34,27,34,27,34,27,34,27],[39,59],
 [26,30,26,31,26,32,26,33],[34,30,34,30,34,30,34,30],[34,31,34,31,34,31,34,31],[34,32,34,32,34,32,34,32],[34,33,34,33,34,33,34,33],
@@ -175,7 +176,7 @@ var raiseStat = function(buttonClass, statValue, character) {
   }
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
   $(".container-fluid").hide();
   $("button#playNow").click(function(){
     $(".splash").hide();
@@ -292,14 +293,12 @@ $(document).ready(function(){
               lowerStat(buttonClass, checkedStat, newCharacter);
               var aPage = storyArray[currentPage-1][1]
             }
-            $(".current-story").append($("#" + aPage))
             if (newCharacter.deathCheck() === true) {
               currentPage = storyArray[3][1];
-              console.log(newCharacter, newCharacter.deathCheck())
             } else {
-              console.log(newCharacter, newCharacter.deathCheck())
               currentPage = aPage;
             }
+            $(".current-story").append($("#" + aPage))
             $(".printText").empty();
             showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
           }
@@ -312,12 +311,12 @@ $(document).ready(function(){
               lowerStat(buttonClass, checkedStat, newCharacter);
               var aPage = storyArray[currentPage-1][3]
             }
-            $(".current-story").append($("#" + aPage))
             if (newCharacter.deathCheck() === true) {
               currentPage = storyArray[3][1];
             } else {
               currentPage = aPage;
             }
+            $(".current-story").append($("#" + aPage))
             $(".printText").empty();
             showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
           }
@@ -330,12 +329,12 @@ $(document).ready(function(){
               lowerStat(buttonClass, checkedStat, newCharacter);
               var aPage = storyArray[currentPage-1][5]
             }
-            $(".current-story").append($("#" + aPage))
             if (newCharacter.deathCheck() === true) {
               currentPage = storyArray[3][1];
             } else {
               currentPage = aPage;
             }
+            $(".current-story").append($("#" + aPage))
             $(".printText").empty();
             showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
           }
@@ -348,12 +347,12 @@ $(document).ready(function(){
               lowerStat(buttonClass, checkedStat, newCharacter);
               var aPage = storyArray[currentPage-1][7]
             }
-            $(".current-story").append($("#" + aPage))
             if (newCharacter.deathCheck() === true) {
               currentPage = storyArray[3][1];
             } else {
               currentPage = aPage;
             }
+            $(".current-story").append($("#" + aPage))
             $(".printText").empty();
             showText($(".printText"), $("#" + currentPage + " .hidden").html(), 0);
           }
